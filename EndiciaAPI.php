917624cfd871ce22c62d71df6a819bee1f969738
tairezzzz <tairezzzz@gmail.com>
2015-09-03 12:03:48 +0300

    guzzle xml parse instead of manual

--- EndiciaAPI.php
+++ EndiciaAPI.php
@@ -6,7 +6,6 @@ namespace tairezzzz\endicia;
  * This is just an example.
  */
 
-
 class EndiciaAPI extends \yii\base\Component
 {
     /**
@@ -43,7 +42,7 @@ class EndiciaAPI extends \yii\base\Component
      */
     function __construct($RequesterID, $AccountID, $PassPhrase)
     {
-        $this->client = new Client('https://www.envmgr.com');
+        $this->client = new \GuzzleHttp\Client(['headers' => ['Content-Type' => 'application/x-www-form-urlencoded']]);
         $this->RequesterID = $RequesterID;
         $this->AccountID = $AccountID;
         $this->PassPhrase = $PassPhrase;
@@ -52,13 +51,13 @@ class EndiciaAPI extends \yii\base\Component
     /**
      * Sends A requst for shipping label and info to endicia
      * @param  array $data Array of data to be formated as XML see Endicia documentation
-     * @return array       The Response data from Endicia as an array , endica actually retuns XML
+     * @return array The Response data from Endicia as an array , endica actually returns XML
      */
     public function request_shipping_label($data)
     {
         // Note you may want to associate this in some other way, like with a database ID.
         $this->PartnerTransactionID = substr(uniqid(rand(), true), 0, 10);
-        $xml = '<LabelRequest Test="YES" LabelType="Default" LabelSize="4X6" ImageFormat="GIF">
+        $xml = '<LabelRequest ImageFormat="GIF" Test="YES">
 					<RequesterID>' . $this->RequesterID . '</RequesterID>
 					<AccountID>' . $this->AccountID . '</AccountID>
 					<PassPhrase>' . $this->PassPhrase . '</PassPhrase>
@@ -71,44 +70,9 @@ class EndiciaAPI extends \yii\base\Component
 
         $xml .= '<ResponseOptions PostagePrice="TRUE"/>
 				</LabelRequest>';
-        $data = array("labelRequestXML" => $xml);
-        $request = $this->client->post('/LabelService/EwsLabelService.asmx/GetPostageLabelXML', array(), $data);
-        return $this->send_request($request);
+        $data = ['labelRequestXML' => $xml];
+        $response = $this->client->get('https://labelserver.endicia.com/LabelService/EwsLabelService.asmx/GetPostageLabelXML', ['query' => $data]);
+        return $response->xml();
     }
 
-    /**
-     * Send HTTP Request using Guzzle
-     * @param  object $request THe Request Object to send using guzzle
-     */
-    public function send_request($request)
-    {
-        $response = $request->send();
-        return $this->parse_response($response);
-    }
-
-    /**
-     * Parses the response from the request using guzzle client
-     * @todo
-     * @param  object $response the response object from guzzle
-     * @return array  returns data from response
-     */
-    public function parse_response($response)
-    {
-        $data = $response->getBody();
-        // Note we could not use Guzzle XML method becuase Endicia does not return valid XML it seems
-        $sxe = new SimpleXMLElement($data);
-        if ($sxe->status == 0) {
-            $return_data = array();
-            $return_data['Status'] = (string)$sxe->Status;
-            $return_data['Base64LabelImage'] = (string)$sxe->Base64LabelImage;
-            $return_data['TrackingNumber'] = (string)$sxe->TrackingNumber;
-            $return_data['FinalPostage'] = (string)$sxe->FinalPostage;
-            $return_data['TransactionID'] = (string)$sxe->TransactionID;
-            $return_data['PostmarkDate'] = (string)$sxe->PostmarkDate;
-            $return_data['DeliveryTimeDays'] = (string)$sxe->PostagePrice->DeliveryTimeDays;
-            return $return_data;
-        } else {
-            return array('status' => 'error', 'message' => $sxe->ErrorMessage);
-        }
-    }
 }
